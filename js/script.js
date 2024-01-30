4e340c124583f547ba65dfc6ad6b49b02ece69d1
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-01-30 18:35:08 +0100

    updated footer and body

--- js/script.js
+++ js/script.js
@@ -4,4 +4,40 @@ let navbar = document.querySelector('.header .navbar');
 menu.onclick = () =>{
     menu.classList.toogle('fa-times');
     navbar.classList.toogle('active');
-}
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    spaceBetween:20,
+    grabCursor:true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+});
+
+var swiper = new Swiper(".service-slider", {
+    loop:true,
+    spaceBetween: 20,
+    grabCursor:true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
