f52015a113dafde9270030f4e88f89e7fb7cd196
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-01-30 19:57:35 +0100

    updated js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-let menu = document.querySelector('menu-btn');
+let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
 menu.onclick = () =>{
-    menu.classList.toogle('fa-times');
-    navbar.classList.toogle('active');
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 };
 
 window.onscroll = () =>{
