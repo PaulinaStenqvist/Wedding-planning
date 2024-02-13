4a8b287793617639430e80cd9285acbaf0791941
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-02-13 17:57:36 +0100

    update js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar');
+const toggleButton = document.getElementById('toggleButton');
+const navbar = document.getElementById('navbar');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-};
+toggleButton.addEventListener('click', () => {
+navbar.classList.toggle('active');
+});
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-};
+
+// let menu = document.querySelector('#menu-btn');
+// let navbar = document.querySelector('.header .navbar');
+
+// menu.onclick = () =>{
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// };
+
+// window.onscroll = () =>{
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// };
 
 var swiper = new Swiper(".home-slider", {
     loop:true,
