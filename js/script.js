c507a6a807deb03ec7c168ba850db743f5071782
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-02-14 19:47:54 +0100

    updated alot

--- js/script.js
+++ js/script.js
@@ -6,18 +6,6 @@ navbar.classList.toggle('active');
 });
 
 
-// let menu = document.querySelector('#menu-btn');
-// let navbar = document.querySelector('.header .navbar');
-
-// menu.onclick = () =>{
-//     menu.classList.toggle('fa-times');
-//     navbar.classList.toggle('active');
-// };
-
-// window.onscroll = () =>{
-//     menu.classList.remove('fa-times');
-//     navbar.classList.remove('active');
-// };
 
 var swiper = new Swiper(".home-slider", {
     loop:true,
@@ -27,6 +15,7 @@ var swiper = new Swiper(".home-slider", {
         el: ".swiper-pagination",
         clickable:true,
     },
+    
 });
 
 var swiper = new Swiper(".service-slider", {
